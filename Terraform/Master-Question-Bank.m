d78f126c34efab42009079996ce21203de988304
Sakshika08 <153513642+Sakshika08@users.noreply.github.com>
2026-08-03 01:22:44 +0530

    Update Master-Question-Bank.m

--- Terraform/Master-Question-Bank.m
+++ Terraform/Master-Question-Bank.m
@@ -1,1 +1,22 @@
+Q1: What is a Provider?
 
+A provider is a plugin that enables Terraform to interact with cloud providers and external services through their APIs.
+
+Q2: Is Provider Different from Resource?
+
+Provider
+
+Plugin
+Connects Terraform to AWS/Azure/Docker
+
+Resource: Actual infrastructure object
+
+Provider = AWS connection
+
+Resource = EC2 instance
+
+Q3: What Happens During terraform init?
+Downloads required providers
+Installs provider plugins
+Initializes backend
+Creates .terraform directory
